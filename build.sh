dd12e22d7b8932790f1ce6279e725f8369062514
VSXR <vsnzdev@gmail.com>
2025-05-13 22:21:07 +0200

    render .yml en proceso de despliegue config

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
+pip install --upgrade pip
 pip install -r requirements.txt
+
+# Run Django commands
 python manage.py collectstatic --noinput
 python manage.py migrate
