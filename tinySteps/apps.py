415cd1e3a190239db26b8654043a1680b2a64165
VSXR <vsnzdev@gmail.com>
2025-05-13 12:00:41 +0200

    archivos principales añadidos

--- tinySteps/apps.py
+++ tinySteps/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
+from .registry import GuideType_Registry
 
-
-class TinystepsConfig(AppConfig):
+class TinyStepsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "tinySteps"
+    
+    def ready(self):
+        """Initialize the application
+        This method is called when the application is ready
+        so, we avoid circular imports by importing the registry here!
+        """
+        GuideType_Registry.initialize()
--- tinySteps/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import InfoRequestModel
-
-class InfoRequestForm(forms.ModelForm):
-    class Meta:
-        model = InfoRequestModel
-        fields = ['name', 'email', 'phone', 'message']
